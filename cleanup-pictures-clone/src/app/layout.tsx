c0c677aa7e8afdd69e16039d04591de2e157c31a
ziyeer <fz4503308@gmail.com>
2025-06-21 21:25:49 +0800

    禁用 Stagewise Toolbar 以解决 WebSocket 连接错误

--- cleanup-pictures-clone/src/app/layout.tsx
+++ cleanup-pictures-clone/src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ClientBody from "./ClientBody";
-import StagewiseToolbarClient from "../components/StagewiseToolbarClient";
+// import StagewiseToolbarClient from "../components/StagewiseToolbarClient";
 import { UserProvider } from "../contexts/UserContext";
 
 const geistSans = Geist({
@@ -29,7 +29,7 @@ export default function RootLayout({
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
       <body suppressHydrationWarning className="antialiased">
         <UserProvider>
-          <StagewiseToolbarClient />
+          {/* <StagewiseToolbarClient /> */}
           <ClientBody>{children}</ClientBody>
         </UserProvider>
       </body>
